c8d258b2d6fec293db7bee33b4f7a62eebfab5c8
Agustin-19 <73861277+Agustin-19@users.noreply.github.com>
2024-07-03 19:22:13 -0300

    Update index.ts

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,9 +1,13 @@
 import { PORT } from "./config/envs";
+import dotenv from "dotenv";
 import app from "./server";
 import "reflect-metadata"
 import { AppDataSource } from "./config/dataSource";
 import { preLoadCategories } from "./helpers/preLoadCategories";
 import { preLoadProducts } from "./helpers/preLoadProducts";
+
+dotenv.config();
+const port = process.env.PORT || 4000;
 
 const initialize = async () => {
     console.log("Initializing server");
@@ -11,8 +15,8 @@ const initialize = async () => {
     console.log("Database initialized");
     await preLoadCategories();
     await preLoadProducts();
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
     });
 }
 
