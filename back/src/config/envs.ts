e1c9e8f716260fa36fdb432b44e7cf1387e46900
Agustin-19 <73861277+Agustin-19@users.noreply.github.com>
2024-07-03 18:37:42 -0300

    Update envs.ts

--- back/src/config/envs.ts
+++ back/src/config/envs.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const PORT: number = Number(process.env.PORT) || 3000;
+export const PORT: number = Number(process.env.PORT) || 4000;
 export const DB_NAME: string = process.env.DB_NAME || "proyecto_m4_front";
 export const DB_USER: string = process.env.DB_USER || "postgres";
 export const DB_PASSWORD: string = process.env.DB_PASSWORD || "admin";
